ba3faefc736d2aab6c0c46fc7ad2970188ea7f40
Marcus Bergman <marcus.bergman@dporganizer.com>
2019-01-28 23:28:31 +0100

    Make the backend run

--- src/db.js
+++ src/db.js
@@ -3,22 +3,48 @@ const Pool = require('pg').Pool;
 const { DATABASE_URL, PSQL_SSL } = process.env;
 
 const pool = new Pool({
-	connectionString: `${DATABASE_URL}${PSQL_SSL ? `?ssl=${PSQL_SSL}` : ''}`,
+  connectionString: `${DATABASE_URL}${PSQL_SSL ? `?ssl=${PSQL_SSL}` : ''}`,
 });
 
-function query(input) {
-	return new Promise((res, rej) => {
-		pool.query(input, (err, results) => {
-			if (err) return rej(err);
-			return res(results);
-		});
-	});
+function query(input, params = []) {
+  return new Promise((res, rej) => {
+    pool.query(input, params, (err, results) => {
+      if (err) return rej(err);
+      return res(results);
+    });
+  });
 }
 
-const getTeams = () => query('SELECT * FROM teams ORDER BY id');
+async function getOrCreateTeamIdByName(name) {
+  const { rows } = await query('SELECT id FROM teams WHERE name ILIKE $1', [
+    name,
+  ]);
+  if (rows.length) return rows[0].id;
+
+  const result = await query(
+    'INSERT INTO teams (name) VALUES($1) RETURNING id',
+    [name]
+  );
+  return result.rows[0].id;
+}
+
+const recordGame = ({ winning_team_id, losing_team_id, played_at }) =>
+  played_at
+    ? query(
+        'INSERT INTO games (winning_team_id, losing_team_id, played_at) VALUES($1, $2, $3)',
+        [winning_team_id, losing_team_id, played_at]
+      )
+    : query(
+        'INSERT INTO games (winning_team_id, losing_team_id) VALUES($1, $2)',
+        [winning_team_id, losing_team_id]
+      );
+
+const getTeams = () => query('SELECT * FROM teams ORDER BY name');
 const getGames = () => query('SELECT * FROM games ORDER BY played_at');
 
 module.exports = {
-	getGames,
-	getTeams,
+  getGames,
+  getTeams,
+  getOrCreateTeamIdByName,
+  recordGame,
 };
--- src/server.js
+++ src/server.js
@@ -10,25 +10,43 @@ const port = process.env.PORT || 3000;
 app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(
-	bodyParser.urlencoded({
-		extended: true,
-	})
+  bodyParser.urlencoded({
+    extended: true,
+  })
 );
 
 app.listen(port, () => {
-	console.log(`App running on port ${port}.`);
+  console.log(`App running on port ${port}.`);
 });
 
 app.get('/health', (req, res) => {
-	res.json({ status: 'ok', name, version });
+  res.json({ status: 'ok', name, version });
 });
 
 app.get('/api/games', async (req, res) => {
-	const games = await db.getGames();
-	res.json(games.rows);
+  const games = await db.getGames();
+  res.json(games.rows);
+});
+app.post('/api/games', async (req, res) => {
+  const { winner, loser, played_at } = req.body;
+
+  if (!winner || !loser) {
+    return res.status(400).json({
+      error: 'winner and loser must be set in application/json format',
+    });
+  }
+  const payload = {
+    winning_team_id: await db.getOrCreateTeamIdByName(winner),
+    losing_team_id: await db.getOrCreateTeamIdByName(loser),
+    played_at,
+  };
+
+  await db.recordGame(payload);
+
+  res.json({ recorded: true });
 });
 
 app.get('/api/teams', async (req, res) => {
-	const teams = await db.getTeams();
-	res.json(teams.rows);
+  const teams = await db.getTeams();
+  res.json(teams.rows);
 });
